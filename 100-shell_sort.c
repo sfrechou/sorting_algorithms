139c2c817223d4ace96f59c021df031b1cb58594
Soledad Frechou <solefrechou@hotmail.com>
2021-01-31 21:55:37 +0000

    Add

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,22 +9,21 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t n = 0, i;
-	int j, temp = 0;
+	size_t n = 0, i, j;
+	int temp = 0;
 
 	if (size < 2)
 		return;
 
 	while (n <= size)
-	{
 		n = (n * 3) + 1;
-	}
+
 	while (n > 1)
 	{
 		n = (n - 1) / 3;
 		for (i = 0; i < size - n; i++)
 		{
-			for (j = i + n; j > 0; j -= n)
+			for (j = i + n; j > 0 && j >= n; j -= n)
 			{
 				if (array[j] < array[j - n])
 				{
