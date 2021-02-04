c62ddb00e46031b03903b1ef018cd7e7e0d7e23e
Soledad Frechou <solefrechou@hotmail.com>
2021-02-04 01:23:00 +0000

    Add

--- sort.h
+++ sort.h
@@ -44,5 +44,6 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void merge_up(int *array, size_t size);
 void merge_down(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
