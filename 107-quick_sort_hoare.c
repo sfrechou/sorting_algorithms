a8268bfb0f32ee3acdd95dc536159272c9fac787
Soledad Frechou <solefrechou@hotmail.com>
2021-02-04 01:22:21 +0000

    Add

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * quick_sort_hoare - Implements quick sort algorithm - Hoare
+ *
+ * @array: The array to be sorted
+ * @size: Size of the array
+*/
+void quick_sort_hoare(int *array, size_t size)
+{
+
+}
